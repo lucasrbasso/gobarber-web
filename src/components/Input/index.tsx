dd1a22d764fba976803900f950f621dcb649a510
Lucas Basso <lucas.rbasso@outlook.com>
2021-03-04 16:25:57 -0300

    using tooltip

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -5,10 +5,12 @@ import React, {
     useState,
     useCallback,
 } from 'react';
+
 import { IconBaseProps } from 'react-icons';
+import { FiAlertCircle } from 'react-icons/fi';
 import { useField } from '@unform/core';
 
-import { Container } from './styles';
+import { Container, Error } from './styles';
 
 interface InputProp extends InputHTMLAttributes<HTMLInputElement> {
     name: string;
@@ -41,7 +43,11 @@ const Input: React.FC<InputProp> = ({ name, icon: Icon, ...rest }) => {
     }, [fieldName, registerField]);
 
     return (
-        <Container isFocused={isFocused} isFilled={isFilled}>
+        <Container
+            isErrored={!!error}
+            isFocused={isFocused}
+            isFilled={isFilled}
+        >
             {Icon && <Icon size={20} />}
             <input
                 onFocus={handleInputFocus}
@@ -50,6 +56,11 @@ const Input: React.FC<InputProp> = ({ name, icon: Icon, ...rest }) => {
                 ref={inputRef}
                 {...rest}
             />
+            {error && (
+                <Error title={error}>
+                    <FiAlertCircle color="#c53030" size={20} />
+                </Error>
+            )}
         </Container>
     );
 };
