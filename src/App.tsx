76dcb8325bd1a668ff2626a1448857d8c53ca463
Lucas Basso <lucas.rbasso@outlook.com>
2021-03-04 16:26:14 -0300

    using auth context

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,18 @@ import React from 'react';
 
 import GlobalStyles from './styles/global';
 
-import SignUp from './pages/SignUp';
+// import SignUp from './pages/SignUp';
+
+import { AuthProvider } from './hooks/AuthContext';
+
 import SignIn from './pages/SignIn';
 
 const App: React.FC = () => {
     return (
         <>
-            <SignUp />
+            <AuthProvider>
+                <SignIn />
+            </AuthProvider>
             <GlobalStyles />
         </>
     );
