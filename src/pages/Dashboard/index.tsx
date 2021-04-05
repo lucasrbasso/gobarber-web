5e89a86a5d38967be42cfbcaf2e664ef59d13542
Lucas Basso <lucas.rbasso@outlook.com>
2021-04-05 17:58:11 -0300

    dashboard: header and appointments now working

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,11 +1,65 @@
 import React from 'react';
 
-import { Container } from './styles';
+import { FiClock, FiPower } from 'react-icons/fi';
+import {
+    Container,
+    Header,
+    HeaderContent,
+    Profile,
+    Content,
+    Schedule,
+    NextAppointment,
+    Calendar,
+} from './styles';
+
+import logoImg from '../../assets/logo.svg';
+import { useAuth } from '../../hooks/Auth';
 
 const Dashboard: React.FC = () => {
+    const { signOut, user } = useAuth();
+
     return (
         <Container>
-            <h1>Dashboard</h1>
+            <Header>
+                <HeaderContent>
+                    <img src={logoImg} alt="GoBarber" />
+                    <Profile>
+                        <img src={user.avatar_url} alt={user.name} />
+                        <div>
+                            <span>Bem vindo,</span>
+                            <strong>{user.name}</strong>
+                        </div>
+                    </Profile>
+
+                    <button type="button" onClick={signOut}>
+                        <FiPower />
+                    </button>
+                </HeaderContent>
+            </Header>
+
+            <Content>
+                <Schedule>
+                    <h1>Horários Agendados</h1>
+                    <p>
+                        <span>Hoje</span>
+                        <span>Dia 06</span>
+                        <span>Segunda-feira</span>
+                    </p>
+
+                    <NextAppointment>
+                        <strong>Atendimento a seguir</strong>
+                        <div>
+                            <img src={user.avatar_url} alt="user" />
+                            <strong>Lucas Basso</strong>
+                            <span>
+                                <FiClock />
+                                08:00
+                            </span>
+                        </div>
+                    </NextAppointment>
+                </Schedule>
+                <Calendar />
+            </Content>
         </Container>
     );
 };
