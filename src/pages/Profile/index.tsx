af7b7adbeb431863502ed0266dd61d9efcae15e6
Lucas Basso <lucas.rbasso@outlook.com>
2021-04-07 15:44:41 -0300

    update profile finished

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -20,7 +20,9 @@ import { useAuth } from '../../hooks/Auth';
 interface ProfileFormData {
     name: string;
     email: string;
+    old_password: string;
     password: string;
+    password_confirmation: string;
 }
 
 const Profile: React.FC = () => {
@@ -60,21 +62,64 @@ const Profile: React.FC = () => {
                     email: Yup.string()
                         .required('E-mail obrigatório')
                         .email('Insira um e-mail válido'),
-                    password: Yup.string().min(6, 'No mínimo 6 dígitos'),
+                    old_password: Yup.string(),
+                    password: Yup.string().when('old_password', {
+                        is(val: string | any[]) {
+                            return !!val.length;
+                        },
+                        then: Yup.string()
+                            .min(6, 'No mínimo 6 dígitos')
+                            .required('Campo Obrigatório'),
+                        otherwise: Yup.string(),
+                    }),
+                    password_confirmation: Yup.string()
+                        .when('old_password', {
+                            is(val: string | any[]) {
+                                return !!val.length;
+                            },
+                            then: Yup.string().required('Campo Obrigatório'),
+                            otherwise: Yup.string(),
+                        })
+                        .oneOf(
+                            [Yup.ref('password'), null],
+                            'Senhas devem ser iguais.',
+                        ),
                 });
 
                 await schema.validate(data, {
                     abortEarly: false,
                 });
 
-                await api.post('/users', data);
+                const {
+                    name,
+                    email,
+                    old_password,
+                    password,
+                    password_confirmation,
+                } = data;
 
-                history.push('/');
+                const formData = {
+                    name,
+                    email,
+                    ...(old_password
+                        ? {
+                              old_password,
+                              password,
+                              password_confirmation,
+                          }
+                        : {}),
+                };
+                const response = await api.put('/profile', formData);
+
+                updateUser(response.data);
+
+                history.push('/dashboard');
 
                 addToast({
                     type: 'success',
-                    title: 'Cadastro realizado',
-                    description: 'Você já pode fazer seu login no GoBarber!',
+                    title: 'Perfil atualizado',
+                    description:
+                        'Suas informações do perfil foram atualizadas com sucesso!',
                 });
             } catch (err) {
                 if (err instanceof Yup.ValidationError) {
@@ -86,9 +131,9 @@ const Profile: React.FC = () => {
 
                 addToast({
                     type: 'error',
-                    title: 'Erro na cadastro',
+                    title: 'Erro na atualização',
                     description:
-                        'Ocorreu um erro ao fazer cadastro, tente novamente.',
+                        'Ocorreu um erro ao atualizar seu perfil, tente novamente mais tarde.',
                 });
             }
         },
